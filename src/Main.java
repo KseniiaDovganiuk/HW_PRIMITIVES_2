3abdb028f179cf69f7fdc93fbebc8cb27bd1e8a4
KseniiaDovganiuk <50490901+KseniiaDovganiuk@users.noreply.github.com>
2024-09-19 14:17:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int startingScore = 100; // начальная сумма на счету клиента
+        int replenishment = 800; // сумма пополнения
+        int bonus; // количество бонусных рублей
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
         }
+        int total = startingScore + replenishment + bonus; // итоговый счет
+
+        System.out.println("Бонусные рубли: " + bonus);
+        System.out.println("Итоговый счет: " + total);
+
     }
 }
